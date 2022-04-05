758e4f27fdd47b7a3f3a17644c20ffa44f9233fb
34675 <34675@LAPTOP-58KM7A9R>
2022-04-05 20:27:33 +0200

    c

--- Spring2022/src/main/java/com/needine/spring2022/controller/EmployeeController.java
+++ Spring2022/src/main/java/com/needine/spring2022/controller/EmployeeController.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,11 +32,32 @@ public class EmployeeController {
 		return employeeService.getAllEmployees();
 	}
 	
+	@GetMapping("{id}")
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") long employeeId){
+		return new ResponseEntity<Employee>(employeeService.getEmployeeById(employeeId), HttpStatus.OK);
+	}
+	
 	@PostMapping()
 	public ResponseEntity<Employee> saveEmployee(@RequestBody Employee employee){
 		return new ResponseEntity<Employee>(employeeService.saveEmployee(employee), HttpStatus.CREATED);
 	}
 	
+	@PutMapping("{id}")
+	public ResponseEntity<Employee> updateEmployee(@PathVariable("id") long id
+												  ,@RequestBody Employee employee){
+		return new ResponseEntity<Employee>(employeeService.updateEmployee(employee, id), HttpStatus.OK);
+	}
+	
+	// build delete employee REST API
+	// http://localhost:8080/api/employees/1
+	@DeleteMapping("{id}")
+	public ResponseEntity<String> deleteEmployee(@PathVariable("id") long id){
+		
+		// delete employee from DB
+		employeeService.deleteEmployee(id);
+		
+		return new ResponseEntity<String>("Employee deleted successfully!.", HttpStatus.OK);
+	}
 	
 }
 
