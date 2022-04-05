1a70144def165449a44600ce10f29944a7ebf65a
34675 <34675@LAPTOP-58KM7A9R>
2022-04-05 19:15:47 +0200

    more

---
+++ Spring2022/src/main/java/com/needine/spring2022/Spring2022Application.java
@@ -0,0 +1,13 @@
+package com.needine.spring2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring2022Application.class, args);
+	}
+
+}
