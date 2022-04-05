6c45f63d940ff6ba8d6540bdfedc60064fff4f2b
34675 <34675@LAPTOP-58KM7A9R>
2022-04-05 19:34:56 +0200

    model

--- Spring2022/src/main/java/com/needine/spring2022/model/Employee.java
+++ Spring2022/src/main/java/com/needine/spring2022/model/Employee.java
@@ -1,6 +1,30 @@
 package com.needine.spring2022.model;
 
-public class Employee {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="employees")
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name = "first_name", nullable = false)
+	private String name;
+	
+	@Column(name = "surname")
+	private String surname;
+
+	@Column(name = "email")
+	private String email;
 	
 }
